fba3639981670c23df5a461712894181b84249d6
Benjamin Alard <88625126+BenAlard@users.noreply.github.com>
2024-12-17 12:48:58 +0100

    add autoscaling

--- main.tf
+++ main.tf
@@ -34,6 +34,19 @@ resource "aws_launch_configuration" "example" {
 				EOF
 	user_data_replace_on_change = true
 
+resource "aws_autoscaling_group" "example" {
+	launch_configuration = aws_launch_configuration.example.name
+
+	min_size = 2
+	max_size = 10
+
+	tag {
+		key = "Name"
+		value = "terraform-asg-example"
+		propagate_at_launch = true
+		}
+	}
+
 tags = {
 	Name = "terraform.example"
 	}
