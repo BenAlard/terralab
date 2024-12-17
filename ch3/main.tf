6c3c52246624c2eb5fdc7c43d534996ddec30867
Benjamin Alard <88625126+BenAlard@users.noreply.github.com>
2024-12-17 12:48:58 +0100

    add s3 backend output

--- ch3/main.tf
+++ ch3/main.tf
@@ -60,3 +60,14 @@ terraform {
 	encrypt		= true
 	}
 }
+
+output "s3_bucket_arn" {
+	value		= aws_s3_bucket.terraform_state.arn
+	description	= "ARN of the s3 bucket"
+}
+
+output "dynamodb_table_name" {
+	value		= aws_dynamodb_table.terraform_locks.name
+	description	= "Name of the dynamodb table"
+}
+
