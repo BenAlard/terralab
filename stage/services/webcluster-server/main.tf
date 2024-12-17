7fa7e592c29731116f4798323b2cb408d99823a7
Benjamin Alard <88625126+BenAlard@users.noreply.github.com>
2024-12-17 12:48:46 +0100

    echo db port and address

--- stage/services/webcluster-server/main.tf
+++ stage/services/webcluster-server/main.tf
@@ -58,6 +58,8 @@ resource "aws_launch_configuration" "example" {
 	user_data	=	<<-EOF
 				#!/bin/bash
 				echo "Hello, World" > index.html
+				echo "${data.terraform_remote_state.db.outputs.address}" >> index.html
+				echo "${data.terraform_remote_state.db.outputs.port}" >> index.html
 				nohup busybox httpd -f -p ${var.server_port} &
 				EOF
 	lifecycle {
