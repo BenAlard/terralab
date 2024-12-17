a6c452d9312d9fb1f32972ab9731c5c24a897459
Benjamin Alard <88625126+BenAlard@users.noreply.github.com>
2024-12-17 12:48:58 +0100

    add target group and health check

--- main.tf
+++ main.tf
@@ -65,6 +65,9 @@ resource "aws_autoscaling_group" "example" {
 	launch_configuration = aws_launch_configuration.example.name
 	vpc_zone_identifier = data.aws_subnets.default.ids
 
+	target_group_arns = [aws_lb_target_group.asg.arn]
+	health_check_type = "ELB"
+
 	min_size = 2
 	max_size = 3
 
@@ -94,6 +97,23 @@ resource "aws_lb_listener" "http" {
 			message_body = "404:page not found"
 			status_code = 404
 		}
+	}
+}
+
+resource "aws_lb_target_group" "asg" {
+	name = "terraform-asg-example"
+	port = var.server_port
+	protocol = "HTTP"
+	vpc_id = data.aws_vpc.default.id
+
+	health_check {
+		path = "/"
+		protocol = "HTTP"
+		matcher = "200"
+		interval = 15
+		timeout = 3
+		healthy_threshold = 2
+		unhealthy_threshold = 2
 	}
 }
 
