7f02ae4825e632303924e1be8ffc5dad17c0d3dc
Benjamin Alard <88625126+BenAlard@users.noreply.github.com>
2024-12-17 12:48:36 +0100

    fix: full path to user-data.sh

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -51,7 +51,7 @@ resource "aws_launch_configuration" "example" {
 	image_id 		=	"ami-03cc8375791cb8bcf"
 	instance_type 	=	"t2.micro"
 	security_groups =	[aws_security_group.instance.id]
-	user_data	=	templatefile("user-data.sh", {
+	user_data	=	templatefile("/home/ben/lab/terraform/modules/services/webserver-cluster/user-data.sh", {
 		server_port	= var.server_port
 		db_address	= data.terraform_remote_state.db.outputs.address
 		db_port		= data.terraform_remote_state.db.outputs.port
