b2ca54c92c6f73ae3dda7c082f9b5a4aa4a26095
Benjamin Alard <88625126+BenAlard@users.noreply.github.com>
2024-12-17 10:43:27 +0100

    remove double dns name

--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -7,8 +7,3 @@ output "asg_name" {
 	value		= aws_autoscaling_group.example.name
 	description	= "Name of the auto scaling group"
 }
-
-output "alb_dns_name" {
-	value		= aws_lb.example.dns_name
-	description	= "Domain name of the load balancer"
-}
