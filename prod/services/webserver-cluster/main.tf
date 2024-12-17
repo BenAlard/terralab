6d60d37333d53f7ce892741147cdf23331e6b051
Benjamin Alard <88625126+BenAlard@users.noreply.github.com>
2024-12-17 10:27:03 +0100

    add scale-in/out

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -12,5 +12,20 @@ module "webserver_cluster" {
 	instance_type		= "t2.micro"
 	min_size		= 2
 	max_size		= 10
+}
 
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+	scheduled_action_name	= "scale_out_during_business_hours"
+	min_size		= 2
+	max_size		= 10
+	desired_capacity	= 10
+	recurrence		= "0 9 * * *"
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+	scheduled_action_name	= "scale_in_at_night"
+	min_size		= 2
+	max_size		= 10
+	desired_capacity	= 2
+	recurrence		= "0 17 * * *"
 }
