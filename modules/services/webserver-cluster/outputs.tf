e3fa8157d81b939efa647390b1b89f49382a8867
Benjamin Alard <88625126+BenAlard@users.noreply.github.com>
2024-12-17 10:38:19 +0100

    add outputs for alb dns names

--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -2,3 +2,13 @@ output	"alb_dns_name" {
 	value = aws_lb.example.dns_name
 	description = "Domain name of the load balancer"
 }
+
+output "asg_name" {
+	value		= aws_autoscaling_group.example.name
+	description	= "Name of the auto scaling group"
+}
+
+output "alb_dns_name" {
+	value		= aws_lb.example.dns_name
+	description	= "Domain name of the load balancer"
+}
